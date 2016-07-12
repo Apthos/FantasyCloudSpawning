4840689f3582de1336dd36a0248e6dea05dc1f6d
Apthos <Kevin@Apthos.net>
2016-07-12 15:23:33 -0700

    removed item entities

--- Fcspawning.java
+++ Fcspawning.java
@@ -3,7 +3,7 @@ package net.apthos.fcspawning;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -17,32 +17,53 @@ public final class Fcspawning extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onSpawn(EntitySpawnEvent e){
+    public void onSpawn(EntitySpawnEvent e) {
 
         Entity entity = e.getEntity();
 
-        boolean found = false;
+        if (entity.getType() == EntityType.BAT || entity.getType() == EntityType.BLAZE ||
+                entity.getType() == EntityType.CAVE_SPIDER || entity.getType() == EntityType.CHICKEN ||
+                entity.getType() == EntityType.COW || entity.getType() == EntityType.CREEPER ||
+                entity.getType() == EntityType.ENDER_DRAGON || entity.getType() == EntityType.ENDERMAN ||
+                entity.getType() == EntityType.ENDERMITE || entity.getType() == EntityType.GHAST ||
+                entity.getType() == EntityType.GUARDIAN || entity.getType() == EntityType.HORSE ||
+                entity.getType() == EntityType.IRON_GOLEM || entity.getType() == EntityType.MAGMA_CUBE ||
+                entity.getType() == EntityType.MUSHROOM_COW || entity.getType() == EntityType.OCELOT ||
+                entity.getType() == EntityType.PIG || entity.getType() == EntityType.PIG_ZOMBIE ||
+                entity.getType() == EntityType.RABBIT || entity.getType() == EntityType.SHEEP ||
+                entity.getType() == EntityType.SILVERFISH || entity.getType() == EntityType.SKELETON ||
+                entity.getType() == EntityType.SLIME || entity.getType() == EntityType.SNOWMAN ||
+                entity.getType() == EntityType.SPIDER || entity.getType() == EntityType.SQUID ||
+                entity.getType() == EntityType.VILLAGER || entity.getType() == EntityType.WITCH ||
+                entity.getType() == EntityType.WOLF || entity.getType() == EntityType.WITHER ||
+                entity.getType() == EntityType.ZOMBIE) {
 
-        Location L = entity.getLocation();
-        Location Min = L.clone().subtract( 9, 9, 9 );
+            boolean found = false;
 
-        for( int y = 0; y < 18; y++ ){
-            for( int x = 0; x < 18; x++ ){
-                for( int z = 0; z < 18; z++ ){
-                    if( Min.clone().add(x,y,z).getBlock().getType().equals(Material.MOB_SPAWNER ) ){
-                        found = true;
-                    }
-                }// end of z
-            }// end of x
-        }// end of y
+            Location L = entity.getLocation();
+            Location Min = L.clone().subtract(9, 9, 9);
 
-        if( !found ) {
-            e.setCancelled( true );
-            entity.remove();
+            for (int y = 0; y < 18; y++) {
+                for (int x = 0; x < 18; x++) {
+                    for (int z = 0; z < 18; z++) {
+                        if (Min.clone().add(x, y, z).getBlock().getType().equals(Material.MOB_SPAWNER)) {
+                            found = true;
+                        }
+                    }// end of z
+                }// end of x
+            }// end of y
+
+            if (!found) {
+                e.setCancelled(true);
+                entity.remove();
+            }
+
         }
-
-
         return;
+    }
+
+    public void CB(EntityType e) {
+
     }
 
 
